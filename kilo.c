a1a7e096aca0afd6a05d651016eb0fa14acdaa97
dylanrainwater <dylanrainwater98@gmail.com>
2020-08-03 15:37:09 -0400

    Turn off echo mode

--- kilo.c
+++ kilo.c
@@ -1,6 +1,36 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    tcgetattr(STDIN_FILENO, &raw);
+    
+    // Turn off echoing
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
-    int x = 1;
-    return x;
+    enableRawMode();
+
+    char c;
+    int size = 0;
+    // Get user input
+    while (read(STDIN_FILENO, &c, 1) && c != 'q') {
+        size++;
+    }
+
+    printf("size=%d\n", size);
+    return 0;
 }
