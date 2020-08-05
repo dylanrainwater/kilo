6ebb7b7628d5419dfb0431b644b78c343ac6994a
dylanrainwater <dylanrainwater98@gmail.com>
2020-08-04 21:12:03 -0400

    Add home/end key support

--- lb.c
+++ lb.c
@@ -18,7 +18,9 @@ enum editorKey {
     ARROW_UP,
     ARROW_DOWN,
     PAGE_UP,
-    PAGE_DOWN
+    PAGE_DOWN,
+    HOME_KEY,
+    END_KEY
 };
 
 /*** data ***/
@@ -85,36 +87,50 @@ int editorReadKey() {
         }
     }
 
+    // Check for command sequence
     if (c == '\x1b') {
         char seq[3];
         
+        // Assume <esc> if nothing after initial seq
         if (read(STDIN_FILENO, &seq[0], 1) != 1) {
             return '\x1b';
-
         }
-
         if (read(STDIN_FILENO, &seq[1], 1) != 1) {
             return '\x1b';
         }
 
+        
         if (seq[0] == '[') {
+            // Check for quick jump commands
             if (seq[1] >= '0' && seq[1] <= '9') {
                 if (read(STDIN_FILENO, &seq[2], 1) != 1) {
                     return '\x1b';
                 }
                 if (seq[2] == '~') {
                     switch(seq[1]) {
+                        case '1': return HOME_KEY;
+                        case '2': return END_KEY;
                         case '5': return PAGE_UP;
                         case '6': return PAGE_DOWN;
+                        case '7': return HOME_KEY;
+                        case '8': return END_KEY;
                     }
                 }
             } else {
+                // Check for arrow keys
                 switch(seq[1]) {
                     case 'A': return ARROW_UP;
                     case 'B': return ARROW_DOWN;
                     case 'C': return ARROW_RIGHT;
                     case 'D': return ARROW_LEFT;
+                    case 'H': return HOME_KEY;
+                    case 'F': return END_KEY;
                 }
+            }
+        } else if (seq[0] == 'O') {
+            switch (seq[1]) {
+                case 'H': return HOME_KEY;
+                case 'F': return END_KEY;
             }
         }
         
@@ -308,6 +324,13 @@ void editorProcessKeypresses() {
                     editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
                 }
             }
+            break;
+
+        case HOME_KEY:
+            E.cx = 0;
+            break;
+        case END_KEY:
+            E.cx = E.screencols - 1;
             break;
 
         case ARROW_UP:
